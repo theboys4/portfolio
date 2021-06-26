1257942dce025af25a695880b3ad0dad10d12f65
Jayasundar <jayasundar.pit@gmail.com>
2021-06-26 00:59:36 -0800

    second commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,45 +11,24 @@ const About = () => {
             <div className='about-main'>
                 <div className='about-main-left'>
                     {/* Sub section 1 */}
-                    <h3 className='about-sub-head'>Teacher</h3>
+                    <h3 className='about-sub-head'>Hello , I'm Jayasundar</h3>
                     <p className='about-details'>
-                        Teaching is my passion, till now, I've taught thousands
-                        of students about Programming and Web Development. Check
-                        out{' '}
-                        <a
-                            className='about-link-element'
-                            href='http://theleanprogrammer.com/aam'
-                        >
-                            my free react courses!
-                        </a>
+                        I'm currently pursuing my undergraduation in Information Technology from Panimalar Institute of Technology{' '}
+                        
                     </p>
 
                     {/* Sub section 2 */}
-                    <h3 className='about-sub-head'>Blogger</h3>
+                    <h3 className='about-sub-head'>Education</h3>
                     <p className='about-details'>
-                        I've been writing blogs from around 3 years now, and
-                        became top-blogger on medium twice. Check out{' '}
-                        <a
-                            className='about-link-element'
-                            href='https://madhavbahl.medium.com/'
-                        >
-                            my medium blogs today!
-                        </a>
+                        Panimalar Institute of Technology    2018-2022  
+                                       </p>
+
+                       <p className='about-details'> Naahar Public School                      2016-2018
+                       
                     </p>
 
                     {/* Sub section 3 */}
-                    <h3 className='about-sub-head'>Public Speaker</h3>
-                    <p className='about-details'>
-                        Being a passionate public speaker, I've given talks at
-                        more than 75 events till now! Want me to speak in your
-                        event?{' '}
-                        <a
-                            className='about-link-element'
-                            href='mailto:info@theleanprogrammer.com'
-                        >
-                            Email me the details!
-                        </a>
-                    </p>
+                   
                 </div>
 
                 <div className='about-main-right'>
